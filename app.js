d4fdad720363abf564258829a9458ec2b4943c7f
Maxence <maxencehedouin@gmail.com>
2023-02-09 15:27:01 +0100

    ajout de yarg et mise en place des master slaves

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
+const os = require('os');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.json({process: process.pid, os: os.hostname()})
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,45 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const app = require('./app')
-const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Serveur en écoute sur le port : ${port}`)
-  })
+const yargs = require('yargs/yargs');
+const { argv } = yargs(process.argv.slice(2))
+    .option('port', {
+        demandOption: false,
+        default: 8000,
+        describe: 'Le port sur lequel le serveur écoute',
+        type: 'number'
+    })
+    .option('address', {
+        demandOption: false,
+        default: '127.0.0.1',
+        describe: 'L\'adresse sur laquelle le serveur écoute',
+        type: 'string'
+
+    })
+
+
+const { port, address } = argv;
+
+const cluster = require('cluster');
+const os = require('os');
+const { log } = require('console');
+
+if (cluster.isMaster) {
+    const numCPUs = os.cpus().length;
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+        
+    }
+    cluster.on('exit', (slave, code, signal) => {
+        console.log(`Worker ${slave.process.pid} died`);
+    }
+    );
+} else {
+    const app = require('./app');
+    console.log(`Worker ${process.pid} started`);
+    app.listen(port, address, () => {
+        console.log(`Serveur en écoute sur le ${address}:${port}`)
+    })
+}
+
+
